f12ec0aebe76cacd0197a2690b11857eda5736de
Worarath Tangsrikeattikul <worarath12@gmail.com>
2018-11-23 16:57:57 +0700

    addlayout and music player (Still bug)

--- app/src/main/java/com/example/l3oom/walkout/MainActivity.java
+++ app/src/main/java/com/example/l3oom/walkout/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.l3oom.walkout;
 
+import android.media.MediaPlayer;
+import android.media.session.MediaSession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaSession mSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AudioPlayer audioPlayer = new AudioPlayer();
+
     }
 }
