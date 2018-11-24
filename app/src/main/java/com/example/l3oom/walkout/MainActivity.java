c6ff9268f2738397537db47e84820a21440b1cdd
Worarath Tangsrikeattikul <worarath12@gmail.com>
2018-11-24 17:35:30 +0700

    add  btn

--- app/src/main/java/com/example/l3oom/walkout/MainActivity.java
+++ app/src/main/java/com/example/l3oom/walkout/MainActivity.java
@@ -1,22 +1,24 @@
 package com.example.l3oom.walkout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
-    AudioPlayer audioPlayer = new AudioPlayer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav_view);
-
+        bottomNavigationView.setItemTextColor(ContextCompat.getColorStateList(this, R.color.bgBottomNavigation));
+        bottomNavigationView.setItemIconTintList(ContextCompat.getColorStateList(this, R.color.bgBottomNavigation));
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -31,6 +33,33 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+        bottomNavigationView.setSelectedItemId(R.id.group_item1);
+
+
+
+        long eventtime = SystemClock.uptimeMillis();
+
+        Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent downEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE, 0);
+        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+        sendOrderedBroadcast(downIntent, null);
+
+        Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent upEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE, 0);
+        upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
+        sendOrderedBroadcast(upIntent, null);
+
+        /*NEXT*/
+        Intent downIntent1 = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent downEvent1 = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN,   KeyEvent.KEYCODE_MEDIA_NEXT, 0);
+        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent1);
+        sendOrderedBroadcast(downIntent1, null);
+
+        /*PREVIOUS*/
+        Intent downIntent2 = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent downEvent2 = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PREVIOUS, 0);
+        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent2);
+        sendOrderedBroadcast(downIntent2, null);
     }
 
 
