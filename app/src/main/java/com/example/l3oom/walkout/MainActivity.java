0e23638ae4a8456f9a7627b386e03b0ea947e793
Worarath Tangsrikeattikul <worarath12@gmail.com>
2018-11-24 21:32:00 +0700

    now it is able to show music track

--- app/src/main/java/com/example/l3oom/walkout/MainActivity.java
+++ app/src/main/java/com/example/l3oom/walkout/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.l3oom.walkout;
 
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.media.Image;
 import android.os.Bundle;
@@ -10,13 +13,26 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static final String SERVICECMD = "com.android.music.musicservicecommand";
+    public static final String CMDNAME = "command";
+    public static final String CMDTOGGLEPAUSE = "togglepause";
+    public static final String CMDSTOP = "stop";
+    public static final String CMDPAUSE = "pause";
+    public static final String CMDPREVIOUS = "previous";
+    public static final String CMDNEXT = "next";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,13 +60,7 @@ public class MainActivity extends AppCompatActivity {
         });
         bottomNavigationView.setSelectedItemId(R.id.group_item1);
 
-
-
-
-
-
-
-        ImageButton playBtn = (ImageButton) findViewById(R.id.playBtn);
+        final ImageButton playBtn = (ImageButton) findViewById(R.id.playBtn);
         playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
                     Intent i = new Intent("com.android.music.musicservicecommand");
                     i.putExtra("command", "play");
                     sendBroadcast(i);
+//                    playBtn.setImageResource(R.drawable.ic);
                 }
             }
         });
@@ -112,8 +123,38 @@ public class MainActivity extends AppCompatActivity {
 //        KeyEvent downEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PREVIOUS, 0);
 //        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
 //        sendOrderedBroadcast(downIntent, null);
+
+        IntentFilter iF = new IntentFilter();
+        iF.addAction("com.android.music.metachanged");
+        iF.addAction("com.android.music.playstatechanged");
+        iF.addAction("com.android.music.playbackcomplete");
+        iF.addAction("com.android.music.queuechanged");
+
+        registerReceiver(mReceiver, iF);
+
     }
 
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @SuppressLint("LongLogTag")
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            TextView nowPlayingTxt = (TextView) findViewById(R.id.now_playing_text);
+            String action = intent.getAction();
+            String cmd = intent.getStringExtra("command");
+            Log.d("mIntentReceiver.onReceive ", action + " / " + cmd);
+            String artist = intent.getStringExtra("artist");
+            String album = intent.getStringExtra("album");
+            String track = intent.getStringExtra("track");
+            Log.d("Music",artist+":"+album+":"+track);
+            nowPlayingTxt.setText(track + " "+album +" "+artist);
+
+        }
+    };
 
 
 }
+
+
+
